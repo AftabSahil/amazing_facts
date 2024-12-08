0d70c9caf3b9b5b9a5f39b43a986d00784461d7b
Sahil Aftab <150364167+AftabSahil@users.noreply.github.com>
2024-12-09 03:00:01 +0530

    Update App.js

--- build/src/App.js
+++ build/src/App.js
@@ -77,14 +77,14 @@ function Header({setForm,showForm})
     )
 }
 const CATEGORIES = [
-  { name: "technology", color: "#3b82f6" },
-  { name: "science", color: "#16a34a" },
-  { name: "finance", color: "#ef4444" },
-  { name: "society", color: "#eab308" },
-  { name: "entertainment", color: "#db2777" },
-  { name: "health", color: "#14b8a6" },
-  { name: "history", color: "#f97316" },
-  { name: "news", color: "#8b5cf6" },
+  { name: "Technology", color: "#3b82f6" },
+  { name: "Science", color: "#16a34a" },
+  { name: "Finance", color: "#ef4444" },
+  { name: "Society", color: "#eab308" },
+  { name: "Entertainment", color: "#db2777" },
+  { name: "Health", color: "#14b8a6" },
+  { name: "History", color: "#f97316" },
+  { name: "News", color: "#8b5cf6" },
 ];
 function isValidUrl(string)
 {
